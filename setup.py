29864f8d3312ee3b8aa4a45337f48337d3fb0b3a
Sergiy Kuzmenko <s.kuzmenko@gmail.com>
2018-10-20 22:05:37 -0400

    Add long description to setup

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@
 from setuptools import setup
 import etlite
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name='etlite',
     version=etlite.VERSION,
     description='Extract/Transform Light - a simple library for reading delimited files.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Sergiy Kuzmenko',
     url='https://github.com/shelldweller/etlite',
     packages=['etlite'],
